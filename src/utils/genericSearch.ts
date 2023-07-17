32809bd8defed635a9ee50782d96a957547251c6
hi-rubygrey <hi.rubygrey@gmail.com>
2023-07-17 23:23:10 +0900

    Searching Over Multiple Properties

--- src/utils/genericSearch.ts
+++ src/utils/genericSearch.ts
@@ -1,8 +1,18 @@
-export const genericSearch = <T>(object: T, property: keyof T, query: string): boolean => {
-    const value = object[property];
-    if (typeof value === "string" || typeof value === "number") {
-        return value.toString().includes(query);
+// if any of the specified properties contain the query string return true
+// otherwise return false.
+export const genericSearch = <T>(object: T, properties: Array<keyof T>, query: string,shouldBeCaseSensitive: boolean = false): boolean => {
+    if (query === "") {
+        return true;
     }
-    return false;
+    return properties.some((property)=>{
+        const value = object[property];
+        if (typeof value === "string" || typeof value === "number") {
+            if (shouldBeCaseSensitive) {
+                return value.toString().includes(query)
+            }
+            return value.toString().toLowerCase().includes(query.toLowerCase());
+        }
+        return false;
+    })
 }
 export default genericSearch
